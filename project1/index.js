9ba3d13112697129819f36d4dd4d842fbd6a84ad
agungTuanany <agung.tuanany@gmail.com>
2020-04-11 01:19:49 +0700

    Parsing Request Path

--- project1/index.js
+++ project1/index.js
@@ -4,17 +4,36 @@
  */
 
 // Dependencies
-const http = require ("http")
+const http	  = require ("http")
+const url	  = require ('url')
 
 //
 const port = 8080
 
 // The server should respond to all request with a string
 const server = http.createServer ((req, res) => {
-	res.end ("Hello world")
+
+	// Get the URL and parse it
+	let parsedUrl =url.parse(req.url, true)
+
+	// Get the path
+	let path = parsedUrl.pathname
+	let trimmedPath = path.replace(/^\/+|\/+$/g, "")
+
+	// Send the response
+	res.end ("Hello world\n")
+
+	// log the request path
+	console.log (`Request recieved on path ${trimmedPath}`)
 })
+
+
+
+
+
+
 
 // Start the server, and have it listen on port
 server.listen (port, () => {
-	console.log (`The server is listening on port ${port}`)
+	console.log (`The server is listening on port ${port} now`)
 })
