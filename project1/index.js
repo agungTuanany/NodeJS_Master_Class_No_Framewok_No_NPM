add6a79c790f5beda28b5b8354917dd5ca05f07b
agungTuanany <agung.tuanany@gmail.com>
2020-04-11 02:19:02 +0700

    Parsing Payloads

--- project1/index.js
+++ project1/index.js
@@ -4,8 +4,9 @@
  */
 
 // Dependencies
-const http	  = require ("http")
-const url	  = require ('url')
+const http			  = require ("http")
+const url			  = require ("url")
+const stringDecoder	  = require ("string_decoder").StringDecoder
 
 //
 const port = 8080
@@ -29,13 +30,27 @@ const server = http.createServer ((req, res) => {
 	// Get the headers as an object
 	const header				= req.headers
 
-	// Send the response
-	res.end ("Hello world\n")
+	// Get the Payload, if any
+	const decoder				= new StringDecoder ("utf-8")
+	let buffer					= ""	// just placeholder for string
 
-	// log the request path
-	console.log (`Request recieved on path: "${trimmedPath}" with method: "${method}"`)
-	console.log ("Query string parameters:", queryStringObject)
-	console.log ("request recieved with thsese headers: ", header)
+	// using Stream
+	req.on ("data", data => {
+		buffer += decoder.write (data)
+	})
+
+	req.on ("end", () => {
+		buffer += decoder.end ()
+
+		// Send the response
+		res.end ("Hello world\n")
+
+		// log the request path
+		//	console.log (`Request recieved on path: "${trimmedPath}" with method: "${method}"`)
+		//	console.log ("Query string parameters:", queryStringObject)
+		//	console.log ("Request recieved with thsese headers: ", header)
+		console.log ("Reqest recived with this payload: ", buffer)
+	})
 })
 
 
