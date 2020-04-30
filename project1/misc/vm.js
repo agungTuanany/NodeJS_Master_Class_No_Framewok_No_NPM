769810d77b1bce3cb88a319e77ccfbf9edf6eaf2
agungTuanany <agung.tuanany@gmail.com>
2020-04-30 18:38:30 +0700

    Chapter_8: Loose Ends | VM (virtual machine)

---
+++ project1/misc/vm.js
@@ -0,0 +1,33 @@
+"user strict"
+/*
+ * Example Virtual Machine
+ * Running Some arbitrary commands
+ *
+ */
+
+
+// Dependencies
+const vm = require ("vm")
+
+
+// Define a context for the script to run in
+const context = {
+	"foo"		: 25
+}
+
+// Define the script
+const script = new vm.Script  (`
+
+	foo = foo * 2
+	var bar = foo + 1
+	var biz = 52
+
+`)
+
+script.runInNewContext (context)
+console.log (context)
+
+
+
+
+
