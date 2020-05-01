90ca9c34ddb2d7799bf3d5a5d8c870aea8846e5f
agungTuanany <agung.tuanany@gmail.com>
2020-05-01 21:11:10 +0700

    Chapter_8: Loose Ends | REPL / NodeJS CLI

---
+++ project1/misc/repl.js
@@ -0,0 +1,27 @@
+"use strict"
+/*
+ * Example REPL server | Node CLI
+ * Take in the world "fizz" and log out "buzz"
+ *
+*/
+
+// Dependencies
+const repl = require ("repl")
+
+// Start the REPL
+repl.start ({
+	"prompt"		: ">",
+	"eval"			: str => {
+		// Evaluation function for incoming inputs
+		console.log ("At the evaluation stage: ", str)
+
+		// If the user said "fizz", say "buzz back to them"
+		if (str.indexOf ("fizz") > -1) {
+			console.log ("buzz")
+		}
+	}
+})
+
+
+
+
